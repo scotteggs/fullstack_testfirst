ad9632a3e4193d24b3a7e00e86a85b0775f4cd02
scotteggs <scotteggs@gmail.com>
2015-08-30 21:56:21 -0700

    first 2 of 7 on 05_loops

--- 05_functions/functions.js
+++ 05_functions/functions.js
@@ -1,1 +1,8 @@
-// Functions.js
+function concat_string () {
+	var arr = [];
+	for (var i = 0; i < arguments.length; i++) {
+	    arr.push(arguments[i]);
+	}
+	return arr.join("");
+	
+}// Functions.js
